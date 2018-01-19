9d03a9305505b1dc5370344662e3fb7bd380c14a
ffzs <dz759043338@163.com>
2018-01-19 15:27:16 +0800

    second

--- lm_pytorch_rnn.py
+++ lm_pytorch_rnn.py
@@ -15,16 +15,13 @@ import visdom
 
 viz = visdom.Visdom()
 
-EPOCHS = 3
+EPOCHS = 20
 BATCH_SIZE = 64
-LR = 0.001
+LR = 0.005
 
 USE_GPU = True
 
-if USE_GPU:
-    gpu_status = torch.cuda.is_available()
-else:
-    gpu_status = False
+gpu_status = torch.cuda.is_available() if USE_GPU else False
 
 train_dataset = datasets.MNIST('./mnist', True, transforms.ToTensor(), download=False)
 test_dataset = datasets.MNIST('./mnist', False, transforms.ToTensor())
@@ -33,33 +30,38 @@ train_loader = DataLoader(train_dataset, BATCH_SIZE, True)
 
 test_data = test_dataset.test_data[:2000]
 test_label =test_dataset.test_labels[:2000]
-
-# viz.images(torch.unsqueeze(test_data[:25], 1), nrow=5)
+# 数据可视化
+viz.images(torch.unsqueeze(test_data[:25], 1), nrow=5)
 
 if gpu_status:
     test_data = test_data.cuda()
-test_data = Variable(test_data, volatile=True).float()/255. # channel 1
+
+test_data = Variable(test_data, volatile=True).float()/255.
 
 class RNN(nn.Module):
     def __init__(self, in_dim, n_class):
         super(RNN, self).__init__()
 
         self.rnn = nn.LSTM(
+            # 输入28 ，输出 64
             input_size=in_dim,
             hidden_size=64,
+            # 神经层 2 层
             num_layers=2,
+            # out(batch, 序列长度, 维度)
             batch_first=True)
+        # n_class为输出的分类数
         self.cf = nn.Linear(64, n_class)
 
     def forward(self, x):
-        # x (batch, time_step ,input_size) out(batch,tiem_step, output_step)
-        # (_,_) (n_layers, batch, hidden_size)
-        out, (_,_ ) = self.rnn(x)
+        # RNN(LSTM)的输出是output 和 hidden 这里只取 output
+        out = self.rnn(x)[0]
+        # 由于LSTM是根据具有对序列的记忆能力，我们只输出序列的最后一位，来判断
         out = out[:, -1, :]
         out = self.cf(out)
         return out
 
-net = RNN(28,10)
+net = RNN(28, 10)
 
 if gpu_status:
     net = net.cuda()
@@ -77,27 +79,25 @@ for epoch in range(EPOCHS):
         if gpu_status:
             tr_x, tr_y = tr_x.cuda(), tr_y.cuda()
         tr_x, tr_y = Variable(tr_x), Variable(tr_y)
-        # forward
         tr_out = net(tr_x)
         loss = loss_f(tr_out, tr_y)
         sum_loss += loss.data[0]*len(tr_y)
         pred_tr = torch.max(tr_out, 1)[1]
         sum_acc += sum(pred_tr==tr_y).data[0]
-
         sum_step += len(tr_y)
-        # backward
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
         if i % 40 == 0:
+            net.eval()
             ts_out = net(test_data)
             pred_ts = torch.max(ts_out, 1)[1].cpu().data
             acc_ts = sum(pred_ts==test_label)/float(test_label.size(0))
             print("epoch: [{}/{}] | Loss: {:.4f} | TR_acc: {:.4f} | TS_acc: {:.4f} | Time: {:.1f}".format(epoch + 1, EPOCHS,
                                                      sum_loss / sum_step, sum_acc / sum_step, acc_ts, time.time() - start_time))
             sum_loss, sum_acc, sum_step = 0., 0., 0.
-torch.save(net.state_dict(), './save/rnn.pth')
+
 
 
 
