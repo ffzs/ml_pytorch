0ea57c3b9d5ab4e6636ca4c4ed2ba288e6408cef
ffzs <dz759043338@163.com>
2018-01-20 10:03:15 +0800

    second

--- ml_pytorch_rnn_char.py
+++ ml_pytorch_rnn_char.py
@@ -19,39 +19,39 @@ import random
 
 USE_GPU = False
 
-if USE_GPU:
-    gpu_status = torch.cuda.is_available()
-else:
-    gpu_status = False
+gpu_status = torch.cuda.is_available() if USE_GPU else False
 
 # string.ascii_letters生成所有字母， string.digits 生成数字
-all_letters = string.ascii_letters+".,:'"
+all_letters = string.ascii_letters+" .,;'"
 n_letters = len(all_letters)
+print(n_letters)
 
 # unicode 转 标准ASCII编码
 def unicode_to_ascii(s):
     s = "".join(c for c in unicodedata.normalize("NFD", s) if unicodedata.category(c) != 'Mn'and c in all_letters)
     return s
 
+# 输入文件获取名字信息
 def readline(filename):
     lines = open(filename, encoding='utf-8').read().strip().split('\n')
     return [unicode_to_ascii(line) for line in lines]
 
 category_lines = {}
 all_categories = []
+# 获取文件夹中所有.txt 文件名
 filenames = glob.glob('data/names/*.txt')
+# {'国家':'[名字]'}的格式所有数据， [国家] 的形式存储label
 for filename in filenames:
     category = filename.split("\\")[-1].split(".")[0]
     all_categories.append(category)
     category_lines[category] = readline(filename)
 n_categories = len(all_categories)
-# print(categroy_lines['Chinese'])
 
 def letter_to_index(letter):
     return all_letters.find(letter)
 
 def letter_to_tensor(letter):
-    tensor = torch.zeros(1,n_letters)
+    tensor = torch.zeros(1, n_letters)
     tensor[0][letter_to_index(letter)] = 1
     return tensor
 
@@ -77,26 +77,24 @@ class RNN(nn.Module):
         return output, hidden
 
     def initHidden(self):
-        if gpu_status:
-            return Variable(torch.ones(1, self.hidden_size)).cuda()
-        else:
-            return Variable(torch.ones(1, self.hidden_size))
+        return Variable(torch.zeros(1, self.hidden_size))
 
 n_hidden = 128
 net = RNN(n_letters, n_hidden, n_categories)
 
+
 if gpu_status:
     net = net.cuda()
     print("#"*30, '使用GPU', '#'*30)
-
+# 获取输出的数据国家
 def category_from_output(output):
-    _ , top = output.data.topk(1)
-    categroy_i = top[0][0]
+    categroy_i = torch.max(output, 1)[1].data[0]
     return all_categories[categroy_i], categroy_i
 
 def random_choice(categories):
     return categories[random.randint(0, len(categories)-1)]
 
+# 随机选取国家进行训练
 def random_train_example():
     category = random_choice(all_categories)
     line = random_choice(category_lines[category])
@@ -104,50 +102,50 @@ def random_train_example():
     line_tensor = Variable(line_to_tensor(line))
     return category, line, category_tensor, line_tensor
 
-
 category, line, category_tensor, line_tensor = random_train_example()
 
 loss_f = nn.NLLLoss()
+optimizer = optim.SGD(net.parameters(), lr=0.001)
 
-def train(category, category_tensor, line_tensor):
-    hidden = net.initHidden()
-    net.zero_grad()
+def train(category, category_tensor, line_tensor,hidden):
     for i in range(line_tensor.size()[0]):
         output, hidden = net(line_tensor[i], hidden)
 
     loss = loss_f(output, category_tensor)
+    optimizer.zero_grad()
     loss.backward()
+    optimizer.step()
 
-    for p in net.parameters():
-        # learning_rate
-        p.data.add_(-0.005, p.grad.data)
-    pred, _ = category_from_output(output)
+    pred = category_from_output(output)[0]
     correct = 1 if pred == category else 0
 
-    return output, loss.data[0], correct
+    return output, loss.data[0], correct, hidden
 
 net.load_state_dict(torch.load('save/char.pth'))
 
 start_time = time.time()
 running_loss, running_acc = 0., 0.
-steps = 100000
+steps = 200000
 print_every = 5000
+# 初始生成一个hidden
+hidden = net.initHidden()
+for step in range(1, steps+1):
+    category, _, category_tensor, line_tensor = random_train_example()
+    if gpu_status:
+        category_tensor = category_tensor.cuda()
+        line_tensor = line_tensor.cuda()
+    output, loss, correct, hidden = train(category, category_tensor, line_tensor, hidden)
+    hidden = Variable(hidden.data)
+    running_loss += loss
+    running_acc += correct
 
-# for step in range(1, steps+1):
-#     category, _, category_tensor, line_tensor = random_train_example()
-#     if gpu_status:
-#         category_tensor = category_tensor.cuda()
-#         line_tensor = line_tensor.cuda()
-#     output, loss, correct = train(category, category_tensor, line_tensor)
-#     running_loss += loss
-#     running_acc += correct
-#
-#     if step % print_every == 0:
-#         print("{} | {:.1f}% | loss: {:.4f}| acc: {:.2f}%| time: {:.2f}".format(step,step/steps*100,running_loss/print_every,
-#                                                                 running_acc/print_every*100,
-#                                                                time.time()-start_time))
-#
-#         running_loss, running_acc = 0., 0.
+    if step % print_every == 0:
+        print("{} | {:.1f}% | loss: {:.4f}| acc: {:.2f}%| time: {:.2f}".format(step,step/steps*100,running_loss/print_every,
+                                                                running_acc/print_every*100,
+                                                               time.time()-start_time))
+
+
+        running_loss, running_acc = 0., 0.
 
 def evaluate(line_tensor):
     hidden = net.initHidden()
@@ -169,6 +167,7 @@ for category in all_categories:
 for i in range(n_categories):
     confusion[i] = confusion[i] / confusion[i].sum()
 
+# 可视化输出
 viz = visdom.Visdom()
 
 viz.heatmap(X=confusion, opts=dict(
